fc295d121e980305cf7a504d9a94c457c204ef38
eugenioclrc <eugenioclrc@gmail.com>
2023-06-05 15:01:33 -0300

    initial commit

---
+++ src/index_spec.ts
@@ -0,0 +1,14 @@
+import "mocha";
+import { expect } from "chai";
+
+import {foo}  from "./index";
+
+describe("RankingHolder", () => {
+  // test case for average here
+
+  describe("highestScore", () => {
+        it("should return null if no result is added", () => {
+            expect(foo).to.be.equal(1);
+        })
+    })
+})
